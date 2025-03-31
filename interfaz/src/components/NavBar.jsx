d2d7f2ad13b36aa6fc51f08147aab4e2622dfeab
zuzano <santi.casalv@hotmail.com>
2025-04-01 00:09:04 +0200

    Añadiendo login

--- interfaz/src/components/NavBar.jsx
+++ interfaz/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Importa Link
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -13,26 +14,28 @@ class NavBar extends React.Component {
     return (
       <Navbar bg="white" expand="lg" fixed="top">
         {/* fluid --> hace que todo el contenedor ocupe todo el ancho posible */}
+        {/* as={Link} to="" --> permite una navegacion dinamica sin necesidad de recargar la pagina a diferencia de href */}
         <Container fluid>
-          <Navbar.Brand href="/"><img
+          <Navbar.Brand as={Link} to="/"><img
                   src={"./../../public/icons/logo.png"}
                   alt="logo"
                   width='200px'
                 /></Navbar.Brand>
-          {/* Es un botón que aparece en pantallas pequeñas (como móviles) para mostrar u ocultar el menú de navegación. */}
+          {/* Es un botón que aparece en pantallas pequeñas (como móviles) para mostrar u ocultar el menú de navegación. Muestra el elemento con un id que
+          coincida con su aria-controls */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="d-flex align-items-center">
-              <Nav.Link href="/about">ABOUT</Nav.Link>
-              <Nav.Link href="/mesas_vip">MESAS VIP</Nav.Link>
-              <Nav.Link href="/entradas">ENTRADAS</Nav.Link>
-              <Nav.Link href="/eventos">EVENTOS</Nav.Link>
-              <Nav.Link href="/contacto">CONTACTO</Nav.Link>
-              <Nav.Link href="/soporte">SOPORTE</Nav.Link>
+              <Nav.Link as={Link} to="/about">ABOUT</Nav.Link>
+              <Nav.Link as={Link} to="/mesasVIP">MESAS VIP</Nav.Link>
+              <Nav.Link as={Link} to="/entradas">ENTRADAS</Nav.Link>
+              <Nav.Link as={Link} to="/eventos">EVENTOS</Nav.Link>
+              <Nav.Link as={Link} to="/contacto">CONTACTO</Nav.Link>
+              <Nav.Link as={Link} to="/soporte">SOPORTE</Nav.Link>
             </Nav>
           </Navbar.Collapse>
           <Nav>
-            <Nav.Link href="/login">INICIAR SESIÓN</Nav.Link>
+            <Nav.Link as={Link} to="/login">INICIAR SESIÓN</Nav.Link> 
           </Nav>
         </Container>
       </Navbar>
