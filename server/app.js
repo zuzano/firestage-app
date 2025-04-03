9c395823a8ca1e71e96589048751a9fa316eaf37
zuzano <santi.casalv@hotmail.com>
2025-04-03 19:38:57 +0200

    Login server

--- server/app.js
+++ server/app.js
@@ -1,23 +1,54 @@
-require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error al conectar a MongoDB:', err));
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error al conectar a MongoDB:', err));
 
-app.get('/api/data', (req, res) => {
-  res.json({ mensaje: 'Datos desde el servidor' });
+// Importar routes
+const usersRouter = require('./routes/usuarioRoutes');
+
+
+const app = express();
+
+
+
+// Permite que tu frontend (como React) haga peticiones al backend desde diferentes dominios.
+app.use(cors());
+// Procesa datos enviados desde formularios HTML. Convierte cosas como "email=ejemplo@correo.com" en un objeto usable.
+app.use(bodyParser.urlencoded({ extended: false }));
+// Procesa datos JSON enviados en las peticiones. Convierte JSON como {"email": "ejemplo@correo.com"} en un objeto de JavaScript.
+app.use(bodyParser.json());
+
+//Archivos estaticos
+app.use(express.static('public'));
+
+// Routes
+app.use('/usuarios', usersRouter);
+
+
+// 404 ERROR
+app.use((req, res, next) => {
+    next(createError(404, 'Recurso no encontrado'));
 });
 
-app.get('/test', (req, res) => {
-  res.send('¡Servidor funcionando!');
+//Manejador de errores
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    res.status(status).json({
+        error: {
+            message: err.message,
+            status: status
+        }
+    });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
