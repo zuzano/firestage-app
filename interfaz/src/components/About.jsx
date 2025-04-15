c85b8230aa4136e7e1bda7a248d461ee499c8b2d
Santiago Castañeda <santi.casalv@hotmail.com>
2025-04-15 21:18:02 +0200

    Añadiendo paginas

---
+++ interfaz/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Button, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function About(){
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default About;
