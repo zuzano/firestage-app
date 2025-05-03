38917da962193471450cefcd35307a521daddae6
zuzano <santi.casalv@hotmail.com>
2025-05-03 14:04:24 +0200

    Subir todo

--- server/routes/premiosRoutes.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-const ctrlPremios = require("../controllers/premios.js");
-
-router.post('/anadirPremios',ctrlPremios.añadirPremios);
-router.get('/mostrarPremios',ctrlPremios.mostrarPremios);
-
-
-module.exports = router;
