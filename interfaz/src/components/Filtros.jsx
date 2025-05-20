927f06d2edee0d1b899b6e4d83b93faa55f59bd0
zuzano <santi.casalv@hotmail.com>
2025-05-20 15:31:33 +0200

    Menu personalizado

--- interfaz/src/components/Filtros.jsx
+++ interfaz/src/components/Filtros.jsx
@@ -6,8 +6,9 @@ import {
     DropdownItem,
     DropdownDivider,
     Button,
+    Form
 } from "react-bootstrap";
-import { Typeahead } from 'react-bootstrap-typeahead';
+import { MenuItem, Menu, Typeahead } from 'react-bootstrap-typeahead';
 import { Icon } from "@iconify/react";
 
 import './../css/filtros.css'
@@ -15,6 +16,7 @@ import './../css/filtros.css'
 function Filtros({ datos, propiedad }) {
 
     const [datoSeleccionado, setDatoSeleccionado] = useState([])
+    const [allSelected, setAllSelected] = useState(true);
 
     const ordenarMenor = (tipoDato) => {
 
@@ -25,23 +27,68 @@ function Filtros({ datos, propiedad }) {
 
     }
 
+    const handleCheckboxChange = (option) => {
+        /*  setDatoSeleccionado(prev => {
+             if (prev.includes(option)) {
+                 return prev.filter(item => item !== option);
+             }
+             return [...prev, option];
+         });
+         setAllSelected(false); */
+    };
+
+    const handleSelectAll = () => {
+        /*   if (allSelected) {
+              setDatoSeleccionado([]);
+          } else {
+              setDatoSeleccionado([...datos]);
+          }
+          setAllSelected(!allSelected); */
+    };
+
     return (
         <>
             <Dropdown >
                 <DropdownToggle variant="custom" className="dropdownToggle d-flex justify-content-between align-items-center" style={{ width: '100%' }}>
-                    <p className="m-0 fw-bold" style={{textTransform:'capitalize'}}>{propiedad} <Icon icon="fluent:filter-12-filled" width="20" height="20" /></p><p className="m-0"><Icon icon="simple-line-icons:options-vertical" width="20" height="20"/> </p>
+                    <p className="m-0 fw-bold" style={{ textTransform: 'capitalize' }}>{propiedad} <Icon icon="fluent:filter-12-filled" width="20" height="20" /></p><p className="m-0"><Icon icon="simple-line-icons:options-vertical" width="20" height="20" /> </p>
                 </DropdownToggle>
                 <DropdownMenu className="dropdownMenu">
-                        <Typeahead
-                            id="datos-typeahead"
-                            className="typeahead"
-                            labelKey={propiedad}
-                            options={datos}
-                            placeholder="Buscar..."
-                            onChange={setDatoSeleccionado}
-                            selected={datoSeleccionado}
-                            open
-                        />
+                    <Typeahead
+                        id="datos-typeahead"
+                        className="typeahead"
+                        labelKey={propiedad}
+                        options={datos}
+                        placeholder="Buscar..."
+                        onChange={setDatoSeleccionado}
+                        selected={datoSeleccionado}
+                        open
+                        renderMenu={(results, menuProps) => (
+                            <>
+                                <Menu {...menuProps}>
+                                    <MenuItem option={"Seleccionar Todo"} position={0} >
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Seleccionar Todo"
+                                            checked={allSelected}
+                                            onChange={handleSelectAll}
+                                        />
+                                    </MenuItem>
+                                    {results.map((result, index) => (
+                                        <MenuItem option={result} position={index+1} >
+                                            <Form.Check
+                                                key={index}
+                                                type="checkbox"
+                                                label={result[propiedad]}
+                                                checked={datoSeleccionado.includes(result)}
+                                                onChange={() => handleCheckboxChange(result)}
+                                            />
+                                        </MenuItem>
+                                    ))}
+
+                                </Menu>
+                            </>
+                        )}
+                    />
                 </DropdownMenu>
             </Dropdown>
         </>
